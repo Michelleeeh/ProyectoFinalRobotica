f1bdd1a8b88516defc74cfa567f400004054c66b
Michelleeeh <163461939+Michelleeeh@users.noreply.github.com>
2026-06-13 19:16:58 -0400

    Refactor map loading for simple and complex environments

--- Código/controllers/epuck_controller/epuck_controller.py
+++ Código/controllers/epuck_controller/epuck_controller.py
@@ -1,21 +1,22 @@
 from controller import Robot
-
-from mapa import cargar_mapa
+from mapa import cargar_mapa, MAPA_SIMPLE, MAPA_COMPLEJO
 from astar import astar
 
-
 robot = Robot()
-
 TIME_STEP = 64
 
+ENTORNO = "simple"   # "simple" o "complejo"
 
-grid, inicio, meta = cargar_mapa()
+if ENTORNO == "simple":
+    grid, inicio, meta = cargar_mapa(MAPA_SIMPLE)
 
-ruta = astar(
-    grid,
-    inicio,
-    meta
-)
+elif ENTORNO == "complejo":
+    grid, inicio, meta = cargar_mapa(MAPA_COMPLEJO)
+
+else:
+    raise ValueError("Entorno inválido")
+
+ruta = astar(grid, inicio, meta)
 
 print("\n--- MAPA CARGADO ---")
 print("Inicio:", inicio)
@@ -27,10 +28,8 @@ if ruta is None:
     print("No existe ruta.")
 else:
     print("Longitud:", len(ruta))
-
     for punto in ruta:
         print(punto)
-
 
 while robot.step(TIME_STEP) != -1:
     pass
